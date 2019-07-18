594705bcf20879398f748e77e3649bc64b7890ee
Tsuchida Shun <silyun2@gmail.com>
2019-07-18 10:45:12 +0900

    [fix] 船の向きによって受ける風の量を変更しました

--- Assets/Resources/Scripts/Common/リニューアル/LiftingForceCalculation.cs
+++ Assets/Resources/Scripts/Common/リニューアル/LiftingForceCalculation.cs
@@ -28,24 +28,25 @@ class Position
 
 public class LiftingForceCalculation : MonoBehaviour
 {
-	[SerializeField] private float l;								// 揚力（Lift）
-	[SerializeField] private float p = 1;							// 風の密度
-	[SerializeField] private float v;								// 相対速度（Vector）
-	[SerializeField] private float s = 1;							// 帆の面積（Surface）
-	[SerializeField] private float cl = 1.12f;						// 揚力係数（Coeffient of Lift）
-	[SerializeField, Range(0, 1.0f)] private float windPower = 0;	// 風力
-	[SerializeField] private float shipspeed;						// 船の速度
+	[SerializeField] private float l;                               // 揚力（Lift）
+	[SerializeField] private float p = 1;                           // 風の密度
+	[SerializeField] private float v;                               // 相対速度（Vector）
+	[SerializeField] private float s = 1;                           // 帆の面積（Surface）
+	[SerializeField] private float cl = 1.12f;                      // 揚力係数（Coeffient of Lift）
+	[SerializeField, Range(0, 1.0f)] private float windPower = 0;   // 風力
+	[SerializeField] private float shipspeed;                       // 船の速度
 
 	// 風の概念を足す
-	[SerializeField] private float windAngle = 0.0f;	// 風の角度
-	[SerializeField] private float shipAngle = 0.0f;	// 船の角度
-	[SerializeField] private float va;					// 船と風との相対速度
-	[SerializeField] private float windPercent;			// 風倍率
+	[SerializeField] private float windAngle = 0.0f;    // 風の角度
+	[SerializeField] private float shipAngle = 0.0f;    // 船の角度
+	[SerializeField] private float va;                  // 船と風との相対速度
+	[SerializeField] private float windPercent;         // 風倍率
 	[SerializeField] private float WindReceived;        // 受ける風
 
 	// 制御点
 	private Position[] pos;     // 0-45
-	private Position[] pos2;    // 46-180
+	private Position[] pos2;    // 46-90
+	private Position[] pos3;    // 91-180
 
 	public void Start()
 	{
@@ -54,14 +55,22 @@ public class LiftingForceCalculation : MonoBehaviour
 		{
 			new Position(0.0f, 0.0f),
 			new Position(40.0f, 0.0f),
-			new Position(40.0f, 0.0f),
-			new Position(45.0f, 100.0f),
+			new Position(50.0f, 0.0f),
+			new Position(45.0f, 80.0f),
 		};
-		//46～180°
+		//46～90°
 		pos2 = new Position[4]
 		{
-			new Position(45.0f, 100.0f),
-			new Position(80.0f, 60.0f),
+			new Position(45.0f, 80.0f),
+			new Position(70.0f, 100.0f),
+			new Position(80.0f, 110.0f),
+			new Position(90.0f, 120.0f),
+		};
+		//91～180°
+		pos3 = new Position[4]
+		{
+			new Position(90.0f, 120.0f),
+			new Position(100.0f, 110.0f),
 			new Position(150.0f, 30.0f),
 			new Position(180.0f, 30.0f),
 		};
@@ -78,18 +87,18 @@ public class LiftingForceCalculation : MonoBehaviour
 
 		if (WindReceived > shipspeed)
 		{
-			v = WindReceived - shipspeed;										// 相対速度計算
-			l = (p * (v * v) * s * cl) / 2;										// 揚力計算
-			shipspeed = Mathf.Sqrt(Mathf.Pow(l, 2) + Mathf.Pow(shipspeed, 2));	// 合力加算
+			v = WindReceived - shipspeed;                                       // 相対速度計算
+			l = (p * (v * v) * s * cl) / 2;                                     // 揚力計算
+			shipspeed = Mathf.Sqrt(Mathf.Pow(l, 2) + Mathf.Pow(shipspeed, 2));  // 合力加算
 		}
 		// 風力が船の推進力より低いとき　⇒　減速
 		else if (WindReceived < shipspeed)
 		{
-			v = shipspeed - WindReceived;										// 相対速度計算
-			l = (p * (v * v) * s * cl) / 2;										// 揚力計算
-			shipspeed = Mathf.Sqrt(Mathf.Pow(shipspeed, 2) - Mathf.Pow(l, 2));	// 合力減算
+			v = shipspeed - WindReceived;                                       // 相対速度計算
+			l = (p * (v * v) * s * cl) / 2;                                     // 揚力計算
+			shipspeed = Mathf.Sqrt(Mathf.Pow(shipspeed, 2) - Mathf.Pow(l, 2));  // 合力減算
 		}
-		this.transform.Translate(Vector3.forward * -shipspeed * Time.deltaTime * 30);	// 船の進行方向に対して加算
+		this.transform.Translate(Vector3.forward * -shipspeed * Time.deltaTime * 30);   // 船の進行方向に対して加算
 	}
 
 	/// <summary>
@@ -100,21 +109,28 @@ public class LiftingForceCalculation : MonoBehaviour
 		//角度計算...船の角度と風の角度に応じて風力を変化させる
 		shipAngle = Mathf.RoundToInt(this.transform.localEulerAngles.y);
 		va = Mathf.Abs(windAngle - shipAngle);
-		if (va > 180.0f) { va = 360.0f - va; }	// 180度に換算
+		if (va > 180.0f) { va = 360.0f - va; }  // 180度に換算
 
-		// (0%)0 ~ 45(100%)
+		// (0%)0 ~ 45(80%)
 		if (va <= 45.0f)
 		{
 			float b = va / 45.0f;
 			float a = 1.0f - b;
 			windPercent = (Mathf.Pow(a, 3) * pos[0].y + 3 * Mathf.Pow(a, 2) * b * pos[1].y + 3 * a * Mathf.Pow(b, 2) * pos[2].y + Mathf.Pow(b, 3) * pos[3].y) / 100;
 		}
-		// (%)46 ~ 180(30%)
-		else if (va > 45)
+		// (80%)46 ~ 90(120%)
+		else if (45 < va && va <= 90)
 		{
-			float b = (va - 45.0f) / 135.0f;
+			float b = (va - 45.0f) / 45.0f;
 			float a = 1.0f - b;
 			windPercent = (Mathf.Pow(a, 3) * pos2[0].y + 3 * Mathf.Pow(a, 2) * b * pos2[1].y + 3 * a * Mathf.Pow(b, 2) * pos2[2].y + Mathf.Pow(b, 3) * pos2[3].y) / 100;
+		}       
+		// (120%)91 ~ 180(30%)
+		else if (90 < va)
+		{
+			float b = (va - 90.0f) / 90.0f;
+			float a = 1.0f - b;
+			windPercent = (Mathf.Pow(a, 3) * pos3[0].y + 3 * Mathf.Pow(a, 2) * b * pos3[1].y + 3 * a * Mathf.Pow(b, 2) * pos3[2].y + Mathf.Pow(b, 3) * pos3[3].y) / 100;
 		}
 	}
 
