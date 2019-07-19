794d9d2d8c260da9a34115b8731187b0d35836d8
SoutaN1673 <souta1673.yda@gmail.com>
2019-07-19 09:52:19 +0900

    [add] リニューアル版の新規シーンの作成
    リニューアル版の新規シーンの作成

---
+++ Assets/Renewal/Script/NewMain/SoloPlay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SoloPlay : MonoBehaviour {
+    
+    [SerializeField]private GameObject SoloPlay00;
+    [SerializeField]private GameObject MainCamera;
+    [SerializeField]private GameObject Menu01;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))  // ソロプレイボタンが押されるとメインカメラに映す
+        {
+            if (MainCamera.transform.position.x >= SoloPlay00.transform.position.x)
+            {
+                SoloPlay00.transform.position = new Vector3(transform.position.x - 1, 0, 0);
+                Menu01.transform.position = new Vector3(transform.position.x - 1, 0, 0);
+            }
+        }
+
+        /*void OnBecameInvisible()
+        {
+            // 表示されなくなった時の処理
+            Destroy("Menu01");
+        }*/
+
+    }
+}
