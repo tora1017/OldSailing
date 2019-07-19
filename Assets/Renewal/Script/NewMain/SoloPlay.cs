9af0c470c7506f8fb138e7cd38df23815816e5c8
SoutaN1673 <souta1673.yda@gmail.com>
2019-07-19 15:33:20 +0900

    [add] メニュー画面の処理の更新

--- Assets/Renewal/Script/NewMain/SoloPlay.cs
+++ Assets/Renewal/Script/NewMain/SoloPlay.cs
@@ -8,23 +8,72 @@ public class SoloPlay : MonoBehaviour {
     [SerializeField]private GameObject SoloPlay00;
     [SerializeField]private GameObject MainCamera;
     [SerializeField]private GameObject Menu01;
-	
-	// Update is called once per frame
-	void Touch () {
-        if (Input.GetMouseButtonUp(0))  // ソロプレイボタンが押されるとメインカメラに映す
+
+    [SerializeField, Range(0, 10)]
+    float time = 1;
+
+    [SerializeField]
+    Vector3 endPosition;
+
+    private float startTime;
+    private Vector3 startPosition;
+
+    void OnEnable()
+    {
+        if (time <= 0)
         {
-            if (MainCamera.transform.position.x >= SoloPlay00.transform.position.x)
-            {
-                SoloPlay00.transform.position = new Vector3(transform.position.x - 1, 0, 0);
-                Menu01.transform.position = new Vector3(transform.position.x - 1, 0, 0);
-            }
+            transform.position = endPosition;
+            enabled = false;
+            return;
         }
 
-        /*void OnBecameInvisible()
-        {
-            // 表示されなくなった時の処理
-            Destroy("Menu01");
-        }*/
+        startTime = Time.timeSinceLevelLoad;
+        startPosition = transform.position;
+    }
 
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonUp(0))  // ソロプレイボタンが押されるとUIを切り替える
+        {
+            Debug.Log("ok");
+            if (Menu01.transform.position.x >= -500)
+            {
+                Menu01.transform.position += new Vector3(-500f, 0f, 0f);
+                Debug.Log("ok");
+            }
+
+            // ソロプレイモードのUI移動の処理
+            if (SoloPlay00.transform.position.x < )
+            {
+                var diff = Time.timeSinceLevelLoad - startTime;
+                if (diff > time)
+                {
+                    transform.position = endPosition;
+                    enabled = false;
+                }
+
+                var rate = diff / time;
+                transform.position = Vector3.Lerp(startPosition, endPosition, rate);
+            }
+        }
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+#if UNITY_EDITOR
+
+        if (!UnityEditor.EditorApplication.isPlaying || enabled == false)
+        {
+            startPosition = transform.position;
+        }
+
+        UnityEditor.Handles.Label(endPosition, endPosition.ToString());
+        UnityEditor.Handles.Label(startPosition, startPosition.ToString());
+#endif
+        Gizmos.DrawSphere(endPosition, 0.1f);
+        Gizmos.DrawSphere(startPosition, 0.1f);
+
+        Gizmos.DrawLine(startPosition, endPosition);
     }
 }
