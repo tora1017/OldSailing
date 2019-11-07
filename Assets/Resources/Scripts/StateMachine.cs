c5c46f6220ed16cdf718e1a1dcb99afc99847ed7
silyun2@gmail.com <silyun2@gmail.com>
2019-11-07 15:30:15 +0900

    add Statemachine
    can`t callback. next try

---
+++ Assets/Resources/Scripts/StateMachine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public interface IStateMachine { }
+
+[SerializeField]
+public class StateMachine<TState> : IStateMachine where TState : struct
+{
+	public TState currentState { get; private set; }
+	public TState prevState { get; private set; }
+
+	public void ChangeState(TState state)
+	{
+		prevState = currentState;
+		currentState = state;
+	}
+
+	public void ChangePrevState()
+	{
+		TState prev = currentState;
+		currentState = prevState;
+		prevState = prev;
+	}
+
+	public void Dispose()
+	{
+		Debug.Log("StateMachine Disposed !");
+	}
+
+	public StateMachine()
+	{
+	}
+
+}
